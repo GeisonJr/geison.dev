4befbddb738c872fb6322f460293dcbf9deb67f0
Geison Oriani <eu@geison.dev>
2025-08-02 19:53:39 -0300

    Refactor API and types for improved request handling and type safety

--- src/helpers/api/index.ts
+++ src/helpers/api/index.ts
@@ -1,4 +1,16 @@
-import type { RequestOptions } from './types'
+import type {
+	RequestOptions,
+	RequestWithoutBodyAndMethod,
+	RequestWithoutMethod,
+	TBody,
+	TBodyInit,
+	THeaders,
+	THeadersInit,
+	TQuery,
+	TQueryInit,
+	TURL,
+	TURLInit
+} from './types'
 
 function isBlob(value: unknown): value is Blob {
 	return value instanceof Blob
@@ -7,103 +19,245 @@ function isBlob(value: unknown): value is Blob {
 function isFormData(value: unknown): value is FormData {
 	return value instanceof FormData
 }
-
 class API {
-	private _baseUrl: URL
+	private baseUrl: TURL
+	private url: TURL | null = null
+	private baseHeaders: THeaders
+	private headers: THeaders | null = null
+	private query: TQuery | null = null
+	private body: TBody | null = null
 
-	constructor(baseUrl: string | URL) {
-		this._baseUrl = this.resolveBaseUrl(baseUrl)
+	constructor(
+		baseUrl: TURLInit,
+		options?: {
+			headers?: THeadersInit
+		}
+	) {
+		this.baseUrl = this.resolveBaseUrl(baseUrl)
+		this.baseHeaders = this.resolveHeaders(options?.headers)
 	}
 
-	private resolveBaseUrl(baseUrl: string | URL): URL {
-		if (!URL.canParse(baseUrl)) {
-			throw new Error(`Invalid base URL: ${baseUrl}`)
-		}
-
+	private resolveBaseUrl(baseUrl: TURLInit): TURL {
 		return new URL(baseUrl)
 	}
 
-	private async request<T>(endpoint: string, options?: RequestOptions): Promise<T> {
+	private resolveHeaders(headers?: THeadersInit): THeaders {
+		return new Headers(headers)
+	}
+
+	private prepareHeaders(headers?: THeadersInit) {
+		this.headers = new Headers()
+
+		// Copy base headers
+		for (const [key, value] of this.baseHeaders.entries()) {
+			this.headers.set(key, value)
+		}
+
+		// Copy headers from request
+		const newHeaders = new Headers(headers)
+		for (const [key, value] of newHeaders.entries()) {
+			this.headers.set(key, value)
+		}
+	}
+
+	private prepareQuery(query?: TQueryInit) {
+		this.query = new URLSearchParams()
+
+		// Copy base query
+		// ...
+
+		// Copy query from request
+		const newQuery = new URLSearchParams(query)
+		for (const [key, value] of newQuery.entries()) {
+			this.query.set(key, value)
+		}
+	}
+
+	private prepareBody(body?: TBodyInit) {
+		if (isBlob(body)) {
+			this.headers!.delete('Content-Type')
+			this.body = body
+		} else if (isFormData(body)) {
+			this.headers!.delete('Content-Type')
+			this.body = body
+		} else {
+			if (!this.headers!.has('Content-Type')) {
+				this.headers!.set('Content-Type', 'application/json')
+			}
+			this.body = JSON.stringify(body)
+		}
+	}
+
+	private prepareUrl(url: string) {
+		if (url.startsWith('/')) {
+			url = url.slice(1)
+		}
+
+		this.url = new URL(url, this.baseUrl)
+
+		if (this.query instanceof URLSearchParams) {
+			this.url.search = this.query.toString()
+		}
+	}
+
+	private async request<T>(url: string, options?: RequestOptions): Promise<T> {
 
 		const {
 			headers: headersFromOptions,
+			query: queryFromOptions,
 			body: bodyFromOptions,
-			query: queryFromOptions = {},
 			...rest
 		} = options ?? {}
 
-		const url = new URL(endpoint, this._baseUrl)
+		this.prepareHeaders(headersFromOptions)
+		this.prepareQuery(queryFromOptions)
+		this.prepareBody(bodyFromOptions)
+		this.prepareUrl(url)
 
-		for (const [key, value] of Object.entries(queryFromOptions)) {
-			url.searchParams.append(key, String(value))
-		}
+		// await new Promise(resolve => setTimeout(resolve, 5000)) // Simulate delay for debugging
 
-		const headers = new Headers(headersFromOptions)
-		let body: BodyInit | undefined
-		if (isBlob(bodyFromOptions)) {
-			headers.delete('Content-Type')
-			body = bodyFromOptions
-		} else if (isFormData(bodyFromOptions)) {
-			headers.delete('Content-Type')
-			body = bodyFromOptions
-		} else {
-			headers.set('Content-Type', 'application/json')
-			body = JSON.stringify(bodyFromOptions)
-		}
-
-		const response = await fetch(url, {
+		const response = await fetch(this.url!, {
 			...rest,
-			headers,
-			body
+			headers: this.headers!,
+			body: this.body!
 		})
 
 		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`)
+			throw new Error(await response.text())
 		}
 
 		return response.json() as Promise<T>
 	}
 
-	public delete<T>(endpoint: string, options?: Omit<RequestOptions, 'method'>) {
-		return this.request<T>(endpoint, {
+	public delete<T>(
+		url: string,
+		options?: RequestWithoutMethod
+	) {
+		return this.request<T>(url, {
 			...options,
 			method: 'DELETE'
 		})
 	}
 
-	public get<T>(endpoint: string, options?: Omit<RequestOptions, 'method' | 'body'>) {
-		return this.request<T>(endpoint, {
+	public get<T>(
+		url: string,
+		options?: RequestWithoutBodyAndMethod
+	) {
+		return this.request<T>(url, {
 			...options,
 			method: 'GET'
 		})
 	}
 
-	public patch<T>(endpoint: string, options?: Omit<RequestOptions, 'method'>) {
-		return this.request<T>(endpoint, {
+	public patch<T>(
+		url: string,
+		options?: RequestWithoutMethod
+	) {
+		return this.request<T>(url, {
 			...options,
 			method: 'PATCH'
 		})
 	}
 
-	public post<T>(endpoint: string, options?: Omit<RequestOptions, 'method'>) {
-		return this.request<T>(endpoint, {
+	public post<T>(
+		url: string,
+		options?: RequestWithoutMethod
+	) {
+		return this.request<T>(url, {
 			...options,
 			method: 'POST'
 		})
 	}
 
-	public put<T>(endpoint: string, options?: Omit<RequestOptions, 'method'>) {
-		return this.request<T>(endpoint, {
+	public put<T>(
+		url: string,
+		options?: RequestWithoutMethod
+	) {
+		return this.request<T>(url, {
 			...options,
 			method: 'PUT'
 		})
 	}
 }
 
-// const api = new API(process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000/api/')
-// const github = new API('https://api.github.com/')
+function api() {
+	const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000/api/'
+	if (!API_URL) {
+		throw new Error('The env "NEXT_PUBLIC_API_URL" is not set')
+	}
+
+	return new API(API_URL, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+}
+
+function discord() {
+	const API_URL = 'https://discord.com/api/v10/'
+	const API_KEY = process.env.DISCORD_API_KEY ?? ''
+	if (!API_KEY) {
+		throw new Error('The env "DISCORD_API_KEY" is not set')
+	}
+
+	return new API(API_URL, {
+		headers: {
+			'Authorization': `Bot ${API_KEY}`,
+			'Content-Type': 'application/json'
+		}
+	})
+}
+
+function github() {
+	const API_URL = 'https://api.github.com/'
+	const API_KEY = process.env.GITHUB_API_KEY ?? ''
+	if (!API_KEY) {
+		throw new Error('The env "GITHUB_API_KEY" is not set')
+	}
+
+	return new API(API_URL, {
+		headers: {
+			'Authorization': `Bearer ${API_KEY}`
+		}
+	})
+}
+
+function gravatar() {
+	const API_URL = 'https://api.gravatar.com/v3/'
+	const API_KEY = process.env.GRAVATAR_API_KEY ?? ''
+	if (!API_KEY) {
+		throw new Error('The env "GRAVATAR_API_KEY" is not set')
+	}
+
+	return new API(API_URL, {
+		headers: {
+			'Authorization': `Bearer ${API_KEY}`,
+			'Content-Type': 'application/json'
+		}
+	})
+}
+
+function wakatime() {
+	const API_URL = 'https://api.wakatime.com/api/v1/'
+	const API_KEY = process.env.WAKATIME_API_KEY ?? ''
+	if (!API_KEY) {
+		throw new Error('The env "WAKATIME_API_KEY" is not set')
+	}
+
+	return new API(API_URL, {
+		headers: {
+			'Authorization': `Basic ${API_KEY}`,
+			'Content-Type': 'application/json'
+		}
+	})
+}
 
 export {
-	API
+	API,
+	api,
+	discord,
+	github,
+	gravatar,
+	wakatime
 }
 
--- src/helpers/api/types.ts
+++ src/helpers/api/types.ts
@@ -1,10 +1,23 @@
-interface RequestOptions
-  extends Omit<RequestInit, 'body'> {
-  body?: BodyInit | object
-  query?: object
+export type TURLInit = URL | string
+export type TURL = URL
+
+export type THeadersInit = HeadersInit
+export type THeaders = Headers
+
+export type TBodyInit = BodyInit | object
+export type TBody = BodyInit | null
+
+export type TQueryInit = URLSearchParams | Record<string, string>
+export type TQuery = URLSearchParams
+
+export type TOptions = {
+	headers?: THeadersInit
 }
 
-export type {
-  RequestOptions
+export interface RequestOptions extends Omit<RequestInit, 'body'> {
+	query?: TQueryInit
+	body?: TBodyInit
 }
 
+export type RequestWithoutMethod = Omit<RequestOptions, 'method'>
+export type RequestWithoutBodyAndMethod = Omit<RequestOptions, 'body' | 'method'>
