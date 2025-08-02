da8d6b4b6ba21031fc59fbc69000ae8064c6a224
Geison Oriani <eu@geison.dev>
2025-08-02 19:56:49 -0300

    Refactor Layout component to include props for Discord, GitHub, Gravatar, and WakaTime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,19 +11,28 @@ export const viewport: Viewport = {
 	colorScheme: 'dark'
 }
 
-type Props = React.PropsWithChildren
+type Props = React.PropsWithChildren<{
+	readonly discord: React.ReactNode
+	readonly github: React.ReactNode
+	readonly gravatar: React.ReactNode
+	readonly wakatime: React.ReactNode
+}>
 
 const inter = Inter({
 	subsets: ['latin']
 })
 
-export default function Layout({ children }: Props) {
+export default function Layout({ children, discord, gravatar, github, wakatime }: Props) {
 
 	return (
 		<html lang={'en-US'}>
 			<body className={inter.className}>
 				<Theme>
 					{children}
+					{gravatar}
+					{github}
+					{wakatime}
+					{discord}
 				</Theme>
 			</body>
 		</html>
